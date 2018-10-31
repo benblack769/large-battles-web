f42f1bd01e68a74bacf95e29f5d437cef9ec8365
Your Name <you@example.com>
2018-10-30 19:54:22 -0700

    added script editing popup

--- modules/browser_only/single_player.js
+++ modules/browser_only/single_player.js
@@ -20,39 +20,87 @@ function createEL(name,methods){
     return el;
 }
 function createDiv(methods){
-    return createEL("div",methods)
+    return createEL("div", methods)
 }
 function createSpan(methods){
-    return createEL("span",methods)
+    return createEL("span", methods)
+}
+function make_change_script_popup(current_value, verifier, callback){
+    $("#global_text_popup").show()
+    $("#text_edit_textarea").val(current_value)
+
+    $("#text_edit_ok_button").click(function(){
+        $("#text_edit_error_textarea").val("")
+        try{
+            var value = $("#text_edit_textarea").val();
+            var parsed_val = verifier(value)
+            callback(parsed_val)
+            $("#global_text_popup").hide()
+        }
+        catch(e){
+            $("#text_edit_error_textarea").val("ERROR: "+e.message)
+        }
+    })
+    $("#text_edit_cancel_button").click(function(){
+        $("#global_text_popup").hide()
+    })
 }
 class ScriptButton{
     constructor(){
-
+        this.state = {
+            data: {},
+            selected: false,
+            editing: false,
+        }
+        this.should_render = true
     }
+    changeState(newstate){
+        this.state = newstate
+        this.should_render = true
+    }
+    shouldRender(){
+        return this.should_render
+    }
+    selectScript(){
+        console.log("selected")
+        this.changeState(Object.assign({selected:true},this.state))
+    }
+
     render(){
+        this.should_render = false
+        var myChildren = !this.editing ? [] :  [
+            createSpan({
+                className: "script_box_button script_box_edit_button",
+                innerText: "Edit",
+            }),
+            createSpan({
+                innerText:"   ",
+            }),
+            createSpan({
+                className: "script_box_button script_box_delete_button",
+                innerText: "Delete",
+            })
+        ]
         var el = createDiv({
             className: "game_script_box",
-            children: [
-                createSpan({
-                    className: "script_box_button script_box_edit_button",
-                    innerText: "Edit",
-                }),
-                createSpan({
-                    innerText:"   ",
-                }),
-                createSpan({
-                    className: "script_box_button script_box_delete_button",
-                    innerText: "Delete",
-                })
-            ]
+            onclick: this.selectScript.bind(this),
+            children: myChildren,
         })
         return el;
     }
 }
 
 function init_single_player(){
-    var button = new MenuButton()
+    var button = new ScriptButton()
     document.getElementById("single_page_game_overlay").appendChild(button.render())
+
+    /*var obj = JSON.stringify({
+        hithere: 123,
+        bob: "green"
+    },null,4)
+    make_change_script_popup(obj,JSON.parse,function(res_val){
+        console.log(res_val)
+    })*/
 }
 
 module.exports = {
