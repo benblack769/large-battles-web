3c0f74ab170320dc88566f38f375e18b7bd425ef
Your Name <you@example.com>
2018-11-03 16:15:40 -0700

    added lib and edit bar

--- modules/browser_only/single_player.js
+++ modules/browser_only/single_player.js
@@ -18,6 +18,11 @@ function createEL(name,methods){
         }
         delete methods.style
     }
+    if(methods.parent){
+        methods.parent.appendChild(el);
+
+        delete methods.parent;
+    }
     if(methods.children){
         methods.children.forEach(function(child){
             el.appendChild(child)
@@ -45,7 +50,7 @@ function make_change_script_popup(current_value, verifier, callback){
         try{
             var value = $("#text_edit_textarea").val();
             var parsed_val = verifier(value)
-            callback(parsed_val)
+            callback(value)
             $("#global_text_popup").hide()
         }
         catch(e){
@@ -76,18 +81,62 @@ class BaseComponent {
         return []
     }
 }
+class LibPannel extends BaseComponent {
+    constructor(parent, basediv){
+        super(parent, basediv)
+        this.interface_div = createDiv({
+            className: "lib_pannel_container",
+        })
+        basediv.appendChild(this.interface_div)
+        this.state = {
+            js_lib: "",
+        }
+        this.stop_edit()
+    }
+    start_edit(){
+        $(this.interface_div).empty()
+        this.edit_lib_button = createDiv({
+            innerText: "Edit Library",
+            className: "lib_edit_button",
+            parent: this.interface_div,
+            onclick: () => {
+                make_change_script_popup(this.state.js_lib,Function,(js_code) => {
+                    this.state.js_lib = js_code
+                })
+            }
+        })
+        this.edit_button = createDiv({
+            innerText: "Stop Edit",
+            className: "lib_edit_button",
+            parent: this.interface_div,
+            onclick: () => {this.sendMessageUp({type:"STOP_EDIT_MODE"})}
+        })
+    }
+    stop_edit(){
+        $(this.interface_div).empty()
+        this.edit_button = createDiv({
+            innerText: "Edit",
+            className: "lib_edit_button",
+            parent: this.interface_div,
+            onclick: () => {this.sendMessageUp({type:"EDIT_MODE"})}
+        })
+    }
+    messageDown(message){
+        switch(message.type){
+            case "EDIT_MODE": this.start_edit(); break;
+            case "STOP_EDIT_MODE": this.stop_edit(); break;
+        }
+    }
+}
 class ScriptInterface extends BaseComponent {
     constructor(parent, basediv){
         super(parent,basediv)
-        this.interface_div = createDiv({
-            className: "script_container",
-        })
-        basediv.appendChild(this.interface_div)
-        this.mybuttonpannel = new ScriptButtonPannel(this,this.interface_div)
-        //this.edit_overlay = new EditOverlay(this,this.interface_div)
+        this.mybuttonpannel = new ScriptButtonPannel(this,basediv)
+        this.libbuttonpannel = new LibPannel(this,basediv)
+        this.edit_overlay = new EditOverlay(this,basediv)
     }
     children(){
-        return [this.mybuttonpannel, this.edit_overlay]
+        return [this.mybuttonpannel,this.libbuttonpannel,this.edit_overlay]
     }
     messageUp(message){
         switch(message.type){
@@ -102,25 +151,31 @@ class EditOverlay extends BaseComponent {
         this.overlay_div = createDiv({
             className: "game_overlay",
         })
+        basediv.appendChild(this.overlay_div)
         $(this.overlay_div).hide()
         this.overlay_div.onclick = this.overlay_gone.bind(this)
     }
     messageDown(message){
+        console.log(message)
         switch(message.type){
             case "EDIT_MODE": $(this.overlay_div).show(); break;
             case "STOP_EDIT_MODE": $(this.overlay_div).hide(); break;
         }
     }
     overlay_gone(){
-        this.sendMessageUp("STOP_EDIT_MODE")
+        this.sendMessageUp({type:"STOP_EDIT_MODE"})
     }
 }
 class ScriptButtonPannel extends BaseComponent {
     constructor(parent, basediv){
         super(parent, basediv)
+        this.interface_div = createDiv({
+            className: "script_container",
+        })
+        basediv.appendChild(this.interface_div)
         this.buttons = [
-            new ScriptButton(this,basediv),
-            new ScriptButton(this,basediv),
+            new ScriptButton(this,this.interface_div),
+            new ScriptButton(this,this.interface_div),
         ]
     }
     messageUp(message){
@@ -146,9 +201,9 @@ class ScriptButton extends BaseComponent {
         super(parent, basediv)
 
         this.state = {
-            data: {},
+            data: "",
             selected: false,
-            editing: true,
+            editing: false,
         }
         this.mydiv = this.render()
         this.basediv.appendChild(this.mydiv)
@@ -182,11 +237,20 @@ class ScriptButton extends BaseComponent {
         this.basediv.replaceChild(newmydiv,this.mydiv)
         this.mydiv = newmydiv
     }
+    data_edited(new_data){
+        this.state.data = new_data
+    }
     render(){
+        var myself = this
         var myChildren = !this.state.editing ? [] :  [
             createSpan({
                 className: "script_box_button script_box_edit_button",
                 innerText: "Edit",
+                onclick: (function(){
+                    make_change_script_popup(myself.data,Function,function(js_code){
+                        myself.data = js_code
+                    })
+                })
             }),
             document.createTextNode(" "),
             createSpan({
