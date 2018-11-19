56e9ad8514e6b47b929411152947682886ebcc3f
ben black <weepingwillowben@gmail.com>
2018-11-18 21:24:29 -0800

    moved static prossing code

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 browserify modules/client_main.js > static_files/bundle.js
 browserify modules/web_worker_main.js > static_files/web_worker.js
 #uglifyjs -m < static_files/bundle.js > static_files/bundle.min.js
-(cd static_files; python process_html.py .)
+(cd static_files; python ../process_html.py .)
