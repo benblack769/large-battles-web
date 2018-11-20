b1f4aca1913686cf812150252c492b617238726d
Your Name <you@example.com>
2018-11-20 14:55:10 -0700

    created github pages docs folder

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ uglifyjs -m < static_files/bundle.js > static_files/bundle.min.js
 uglifyjs -m < static_files/web_worker.js > static_files/web_worker.min.js
 (cd static_files; python ../process_html.py .)
 gzip -k -f static_files/index.html
+cp static_files/index.html docs/
