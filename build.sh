5a484e87c1dfc8e33036bd90572cf69daa0c64fd
Ben Black <weepingwillowben@gmail.com>
2019-01-09 19:17:29 -0700

    removed cpp code

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 # builds files into a single html file ready for deployment
 browserify modules/client_main.js > static_files/bundle.js
-#(cd cpp_module; bash compile.sh)
 #cp cpp_module/a.out.wasm static_files/
 #browserify modules/web_worker_main.js > static_files/web_worker.js
 #uglifyjs -m < static_files/bundle.js > static_files/bundle.min.js
--- cpp_module/test_compile.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++14 cpp_module/process_prob_map.cpp -D RUN_TEST
