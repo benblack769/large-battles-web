3c525b042826156d95d441a0a7e9b3cccb976444
ben black <weepingwillowben@gmail.com>
2019-12-16 07:20:59 -0500

    turned code into asserts

--- src/validate.cpp
+++ src/validate.cpp
@@ -20,67 +20,47 @@ struct validate_error{
 validate_error valid(){
     return validate_error();
 }
-//#def assert_ret(expr,msg) {if(expr){return validate_error(msg);}}
+#define assert_ret(expr,msg) {if(!(expr)){return validate_error("ERROR: \n" #expr "\n" msg "\n");}}
 #define ret_if(expr) {auto val = (expr); if(val){return val;}}
 validate_error assert_valid_coord(const Map & map,Point coord){
-    if(!map.in_bounds(coord)){
-        return validate_error("Targeted player other than self");
-    }
+    assert_ret(map.in_bounds(coord),"Targeted player other than self");
     return valid();
 }
 validate_error assert_empty(const Map & map, Point coord){
     ret_if(assert_valid_coord(map,coord));
-    if(map[coord].category != Category::EMPTY){
-        return validate_error("Coordinate should be empty, was not");
-    }
+    assert_ret(map[coord].category == Category::EMPTY,"Coordinate should be empty, was not");
     return valid();
 }
 validate_error assert_player_is(const Map & map, Point coord, Player player){
-    if(!is_player(map[coord],player)){
-        return validate_error("Targeted player other than self");
-    }
+    assert_ret(is_player(map[coord],player),"Targeted player other than self");
     return valid();
 }
 validate_error assert_player_is_not(const Map & map, Point coord, Player player){
-    if(is_player(map[coord],player)){
-        return validate_error("Cannot attack yourself");
-    }
+    assert_ret(!is_player(map[coord],player),"Cannot attack yourself");
     return valid();
 }
 validate_error assert_actual_move(Point start,Point end){
-    if(start == end){
-        return validate_error("zero position moves invalid");
-    }
+    assert_ret(start != end,"zero position moves invalid");
     return valid();
 }
 validate_error assert_actual_attack(Point start,Point end){
-    if(start == end){
-        return validate_error("units cannot attack themselves");
-    }
+    assert_ret(start != end,"units cannot attack themselves");
     return valid();
 }
 validate_error assert_is_unit(const Map & map, Point coord){
     ret_if(assert_valid_coord(map,coord));
-    if(map[coord].category != Category::UNIT){
-        return validate_error("Coordinate should be unit, was not");
-    }
+    assert_ret(map[coord].category == Category::UNIT,"Coordinate should be unit, was not");
     return valid();
 }
 validate_error assert_hasnt_moved(const Unit & unit){
-    if(unit.status.moved){
-        return validate_error("tried to move unit twice in a single turn");
-    }
+    assert_ret(!unit.status.moved,"tried to move unit twice in a single turn");
     return valid();
 }
 
 validate_error assert_in_range(const Map & map,Point start_coord,Point end_coord,int range){
     //bool is_possible_move(const Map & map,Point start,Point end,int range){
-    if(distance(start_coord,end_coord) > range){
-        return validate_error("Square out of range, distance too great.");
-    }
-    if(!(is_possible_move(map,start_coord,end_coord,range))){
-        return validate_error("square out of range, blocked by unit.");
-    }
+    assert_ret(distance(start_coord,end_coord) <= range,"Square out of range, distance too great.");
+    assert_ret(is_possible_move(map,start_coord,end_coord,range),"square out of range, blocked by unit.");
     return valid();
 }
 validate_error assert_movement_range(const Game & game,const MoveInfo & instr,const Unit & unit){
@@ -89,16 +69,12 @@ validate_error assert_movement_range(const Game & game,const MoveInfo & instr,co
     return valid();
 }
 validate_error assert_active_player(const Game & game,Player player){
-    if(game.players.active_player != player){
-        return validate_error("You are not the active player and so you can make no moves.");
-    }
+    assert_ret(game.players.active_player == player,"You are not the active player and so you can make no moves.");
     return valid();
 }
 validate_error assert_unit_active(const Map & map,Point coord){
     int num_active_turns = map[coord].status.turns_til_active;
-    if(num_active_turns > 0){
-        return validate_error("Unit needs more turns until active");
-    }
+    assert_ret(num_active_turns == 0,"Unit needs more turns until active");
     return valid();
 }
 validate_error assert_active_unit(const Game & game,Point coord,Player player){
@@ -120,16 +96,13 @@ validate_error valid_move(const Game & game,const MoveInfo & instr,Player player
     return valid();
 }
 validate_error assert_hasnt_attacked(const Unit & unit){
-    if(unit.status.attacked){
-        return validate_error("tried to use unit to attack twice in a single turn");
-    }
+    assert_ret(!unit.status.attacked,"tried to use unit to attack twice in a single turn");
     return valid();
 }
 validate_error assert_is_possible_attack(const Game & game,const AttackInfo & instr,const Unit & unit){
     int range = game.stats.total_stats(unit).attack_range;
-    if(!is_possible_attack(game.map,instr.source_coord,instr.target_coord,range)){
-        return validate_error("Cannot attack target. Remember that attacks can only occur in strait lines and that attacking through a unit is not possible.");
-    }
+    assert_ret(is_possible_attack(game.map,instr.source_coord,instr.target_coord,range),
+               "Cannot attack target. Remember that attacks can only occur in strait lines and that attacking through a unit is not possible.");
     return valid();
 }
 validate_error valid_attack(const Game & game,const AttackInfo & instr,Player player){
@@ -146,29 +119,21 @@ int get_money(const Game & game,Player player){
     return game.players.get(player).money;
 }
 validate_error assert_money_enough(const Game & game,Player player,UnitType build_type){
-    if(game.stats.get(build_type).cost > get_money(game,player)){
-        return validate_error("Building costs more money than you have!");
-    }
+    assert_ret(game.stats.get(build_type).cost <= get_money(game,player),"Building costs more money than you have!");
     return valid();
 }
 validate_error assert_money_enough_equip(const Game & game,Player player,AttachType equip_type){
-    if(game.stats.get(equip_type).stat_alt.cost > get_money(game,player)){
-        return validate_error("Equipment costs more money than you have!");
-    }
+    assert_ret(game.stats.get(equip_type).stat_alt.cost <= get_money(game,player),"Equipment costs more money than you have!");
     return valid();
 }
 validate_error assert_buildable(const Game & game,UnitType build_type){
-    if(!game.stats.get(build_type).buildable){
-        return validate_error("Unit type not buildable!");
-    }
+    assert_ret(game.stats.get(build_type).buildable,"Unit type not buildable!");
     return valid();
 }
 
 validate_error assert_builder(const Game & game,Point coord){
     Unit unit = game.map[coord];
-    if(!game.stats.get(unit.unit_type).builder){
-        return validate_error("annot build over a unit that is not a builder!");
-    }
+    assert_ret(game.stats.get(unit.unit_type).builder,"Cannot build over a unit that is not a builder!");
     return valid();
 }
 
@@ -188,12 +153,8 @@ validate_error valid_end_turn(const Game & game,Player player){
 validate_error assert_building_can_build(const Game & game,const BuyUnitInfo & instr){
     Unit building = game.map[instr.building_coord];
     UnitStat building_stats = game.stats.get(building.unit_type);
-    if(!building_stats.can_make.includes(instr.buy_type)){
-        return validate_error("Selected building cannot make unit of selected type");
-    }
-    if(!building.status.buys_left){
-        return validate_error("Building cannot buy any more units this turn. Wait until next turn.");
-    }
+    assert_ret(building_stats.can_make.includes(instr.buy_type),"Selected building cannot make unit of selected type");
+    assert_ret(building.status.buys_left > 0,"Building cannot buy any more units this turn. Wait until next turn.");
     return valid();
 }
 validate_error valid_buy_unit(const Game & game,const BuyUnitInfo & instr,Player player){
@@ -208,24 +169,16 @@ validate_error valid_buy_unit(const Game & game,const BuyUnitInfo & instr,Player
 validate_error assert_building_can_equip(const Game & game,const BuyAttachInfo & instr){
     Unit building = game.map[instr.building_coord];
     UnitStat building_stats = game.stats.get(building.unit_type);
-    if(!building_stats.can_make_equip.includes(instr.equip_type)){
-        return validate_error("Selected building cannot make unit of selected type");
-    }
-    if(!building.status.buys_left){
-        return validate_error("Building cannot buy any more units this turn. Wait until next turn.");
-    }
+    assert_ret(building_stats.can_make_equip.includes(instr.equip_type),"Selected building cannot make unit of selected type");
+    assert_ret(building.status.buys_left > 0,"Building cannot buy any more equipment this turn. Wait until next turn.");
     return valid();
 }
 validate_error assert_target_can_be_equipped(const Game & game,const BuyAttachInfo & instr){
     Unit target = game.map[instr.equip_coord];
     UnitStat target_stats = game.stats.get(target.unit_type);
-    if(!target_stats.viable_attachments.includes(instr.equip_type)){
-        return validate_error("Target unit cannot equip equipment of selected type");
-    }
+    assert_ret(target_stats.viable_attachments.includes(instr.equip_type),"Target unit cannot equip equipment of selected type");
     SlotType slot = game.stats.get(instr.equip_type).slot;
-    if(target.attachments.slot_filled(slot)){
-        return validate_error("Attachment slot for this unit is already full!.");
-    }
+    assert_ret(!target.attachments.slot_filled(slot),"Attachment slot for this unit is already full!.");
     return valid();
 }
 validate_error valid_buy_attachment(const Game & game,const BuyAttachInfo & instr,Player player){
@@ -239,9 +192,7 @@ validate_error valid_buy_attachment(const Game & game,const BuyAttachInfo & inst
     return valid();
 }
 validate_error validate_game_start(const Game & ,const InitGameInfo & ,Player player){
-    if(player != Player::SERVER_PLAYER){
-        return validate_error("Only server player can issue this special instruction: game_start.");
-    }
+    assert_ret(player == Player::SERVER_PLAYER,"Only server player can issue this special instruction: game_start.");
     return valid();
 }
 validate_error valid_gamemove(const Game & game,const GameMove & instr,Player player){
